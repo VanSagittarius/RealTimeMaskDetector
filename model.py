990cef9b8b97de2a488824f5c69b63f8cc2aab49
Maurycy Czajka <44880057+VanSagittarius@users.noreply.github.com>
2021-01-23 21:34:16 +0100

    Update model.py

--- model.py
+++ model.py
@@ -32,7 +32,7 @@ model = Sequential([
 model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['acc'])
 
 #Image Data Generation/Augmentation:
-TRAINING_DIR = "/train"
+TRAINING_DIR = ".../train"
 train_datagen = ImageDataGenerator(rescale=1.0/255,
                                    rotation_range=40,
                                    width_shift_range=0.2,
@@ -45,7 +45,7 @@ train_datagen = ImageDataGenerator(rescale=1.0/255,
 train_generator = train_datagen.flow_from_directory(TRAINING_DIR, 
                                                     batch_size=10, 
                                                     target_size=(150, 150))
-VALIDATION_DIR = "/test"
+VALIDATION_DIR = ".../test"
 validation_datagen = ImageDataGenerator(rescale=1.0/255)
 
 validation_generator = validation_datagen.flow_from_directory(VALIDATION_DIR, 
